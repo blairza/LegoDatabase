9b6103786c956f6c3ae335eaece2126ef38db2b4
blairza <blairza@rose-hulman.edu>
2020-04-29 01:27:26 -0400

    Got Sql Server stuff for conencting to database and set up conenction class but need LegoDatabaseUi.jar to actually connect

--- src/LegoDatabase.java
+++ src/LegoDatabase.java
@@ -1,5 +1,50 @@
 //Main Class for Lego Database
 //Zane Blair and Alan Bruner
-public class LegoDatabase {
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LegoDatabase {
+	private final String SampleURL = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
+
+	private Connection connection = null;
+
+	private String databaseName;
+	private String serverName;
+
+	public LegoDatabase(String serverName, String databaseName) {
+		this.serverName = serverName;
+		this.databaseName = databaseName;
+	}
+
+	public boolean connect(String user, String pass) {
+		String conStr = SampleURL;
+		conStr = conStr.replace("${dbServer}", this.serverName);
+		conStr = conStr.replace("${dbName}", this.databaseName);
+		conStr = conStr.replace("${user}", user);
+		conStr = conStr.replace("${pass}", pass);
+		try{
+			this.connection = DriverManager.getConnection(conStr);
+			return true;
+	        }
+	        // Handle any errors that may have occurred.
+	        catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+		return false;
+	}
+	
+
+	public Connection getConnection() {
+		return this.connection;
+	}
+	
+	public void closeConnection() {
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
