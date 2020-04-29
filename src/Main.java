dd2c60cb033bc7cc4233bb6a7e1459f7c355f60b
blairza <blairza@rose-hulman.edu>
2020-04-29 15:13:14 -0400

    Moved main file and got it to run

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+	public static void main(String[] args) {
+		LegoDatabase ld = new LegoDatabase("golem.csse.rose-hulman.edu","LegoDatabase");
+		ld.connect("LegoDatabaseUserblairza", "Password123");
+	}
+
+}
